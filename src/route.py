dfbc543ad80536d776b7d2f80436c6115cc1fdd7
paololanza <paolo.lanza.97@gmail.com>
2022-12-28 12:15:04 +0100

    publish endpoint added

--- src/route.py
+++ src/route.py
@@ -1,7 +1,9 @@
 from src import app
 
 import json
+from datetime import datetime
 import time
+import os
 
 from flask import request
 
@@ -9,6 +11,19 @@ import zdm
 
 @app.route("/api/publish", methods=["POST"])
 def publish():
-    data = request.args.get("cred")
-    print(request.data)
-    return "ciao"
+    data = json.loads(request.data)
+    cred = data['cred']
+    payload = data['payload']
+    tag = data['tag']
+
+    # connecting to Zerynth Cloud
+    print(cred)
+    credential = zdm.Credentials(cred)
+    device = zdm.ZDMClient(credential)
+    device.connect()    
+
+    # sending payload to Zerynth Cloud
+    device.publish(payload, tag)
+    time.sleep(5)
+
+    return {"status" : True}
